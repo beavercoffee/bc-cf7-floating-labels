42ed882c0fed85395aa45605899c8899d9835d7a
beavercoffee <beavercoffee@vidsoe.com>
2021-07-14 17:52:14 -0600

    BC

--- bc-cf7-floating-labels.php
+++ bc-cf7-floating-labels.php
@@ -2,20 +2,20 @@
 /*
 Author: Beaver Coffee
 Author URI: https://beaver.coffee
-Description: Add Bootstrap 4 to Contact Form 7 fields.
+Description: Add floating labels to Contact Form 7 fields.
 Domain Path:
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Network: true
-Plugin Name: BC CF7 Bootstrap 4
-Plugin URI: https://github.com/beavercoffee/bc-cf7-bootstrap-4
+Plugin Name: BC CF7 Floating Labels
+Plugin URI: https://github.com/beavercoffee/bc-cf7-floating-labels
 Requires at least: 5.7
 Requires PHP: 5.6
-Text Domain: bc-cf7-bootstrap-4
-Version: 1.7.14
+Text Domain: bc-cf7-floating-labels
+Version: 1.7.14.1
 */
 
 if(defined('ABSPATH')){
-    require_once(plugin_dir_path(__FILE__) . 'classes/class-bc-cf7-bootstrap-4.php');
-    BC_CF7_Bootstrap_4::get_instance(__FILE__);
+    require_once(plugin_dir_path(__FILE__) . 'classes/class-bc-cf7-floating-labels.php');
+    BC_CF7_Floating_Labels::get_instance(__FILE__);
 }
--- readme.txt
+++ readme.txt
@@ -1,20 +1,20 @@
-=== BC CF7 Bootstrap 4 ===
+=== BC CF7 Floating Labels ===
 Contributors: beavercoffee
 Donate link: https://beaver.coffee
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Requires at least: 5.7
 Requires PHP: 5.6
-Stable tag: 1.7.14
+Stable tag: 1.7.14.1
 Tags:
 Tested up to: 5.7.2
 
-BC CF7 Bootstrap 4
+BC CF7 Floating Labels
 
 == Description ==
 
-Add Bootstrap 4 to Contact Form 7 fields.
+Add floating labels to Contact Form 7 fields.
 
 == Changelog ==
 
-[Changelog »](https://github.com/beavercoffee/bc-cf7-bootstrap-4 "BC CF7 Bootstrap 4")
+[Changelog »](https://github.com/beavercoffee/bc-cf7-floating-labels "BC CF7 Floating Labels")
